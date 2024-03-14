acb35015dad093eb39237f7857844ace2c97037a
Degef <Degei411233@outlook.com>
2024-03-14 15:28:15 +0400

    adding updating deleting post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from .views import PostListView,PostDetailView,PostCreateView
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+	path('', PostListView.as_view(), name='home'),
+	path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+	path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
